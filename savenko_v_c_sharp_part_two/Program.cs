0c29adef1455d2c1271581b56257291fd2dd511c
Vadim Savenko <savenko.v@pg.com>
2020-05-28 22:26:52 +0300

    dsfdsf

---
+++ savenko_v_c_sharp_part_two/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savenko_v_c_sharp_part_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sdfsdfsdf
+        }
+    }
+}
---
+++ savenko_vadim_c_sharp_part_two/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace savenko_vadim_c_sharp_part_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
